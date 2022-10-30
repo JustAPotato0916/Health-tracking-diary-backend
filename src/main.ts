9e94d0c8ef3a6b0bc16daac233f9152773d01e59
Atsuma <asd463291@gmail.com>
2022-10-30 09:45:18 +0800

    feature: add firebase and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('健康追蹤日記 API')
+    .setDescription('健康追蹤日記 API 文件')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('/doc', app, document);
+
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
